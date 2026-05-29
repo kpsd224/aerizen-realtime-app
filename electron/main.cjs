4a6e29e2c4f1f408a399baa49e5bc134a4bf7e7c
kpsd224 <kopposindodoc@gmail.com>
2026-05-29 13:25:42 +0700

    Add files via upload

---
+++ electron/main.cjs
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1440,
+    height: 960,
+    minWidth: 1180,
+    minHeight: 760,
+    title: "Aerizen Asset Management",
+    backgroundColor: "#f1f5f9",
+    webPreferences: {
+      contextIsolation: true,
+      nodeIntegration: false,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:5173");
+  } else {
+    win.loadFile(path.join(__dirname, "../dist/index.html"));
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
